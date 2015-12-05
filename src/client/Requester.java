8f1cd18ab5ac8c7b73454285c3ab7efc7cc3c17d
Ross Byrne <ross.byrne@outlook.ie>
2015-12-05 19:31:36 +0000

    Adding Eclipse Project To Repo

---
+++ src/client/Requester.java
@@ -0,0 +1,95 @@
+package client;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Requester{
+	
+	Socket requestSocket;
+	ObjectOutputStream out;
+ 	ObjectInputStream in;
+ 	String message="";
+ 	String ipaddress;
+ 	Scanner stdin;
+ 	
+ 	// Constructor
+	Requester(){
+		
+	} // Requester()
+	
+	void run(){
+		
+		stdin = new Scanner(System.in);
+		try{
+			//1. creating a socket to connect to the server
+			System.out.println("Please Enter your IP Address");
+			ipaddress = stdin.next();
+			requestSocket = new Socket(ipaddress, 2004);
+			System.out.println("Connected to "+ipaddress+" in port 2004");
+			//2. get Input and Output streams
+			out = new ObjectOutputStream(requestSocket.getOutputStream());
+			out.flush();
+			in = new ObjectInputStream(requestSocket.getInputStream());
+			System.out.println("Hello");
+			//3: Communicating with the server
+			do{
+				try{
+						
+					message = (String)in.readObject();
+					System.out.println("Please Enter the Message to send...");
+					message = stdin.next();
+					sendMessage(message);
+					
+				} catch(ClassNotFoundException classNot){
+					
+					System.err.println("data received in unknown format");
+				} // try catch
+				
+			}while(!message.equals("bye")); // do while
+			
+		} catch(UnknownHostException unknownHost){
+			
+			System.err.println("You are trying to connect to an unknown host!");
+			
+		} catch(IOException ioException){
+			
+			ioException.printStackTrace();
+		} finally{
+			
+			//4: Closing connection
+			try{
+				in.close();
+				out.close();
+				requestSocket.close();
+			} catch(IOException ioException){
+				
+				ioException.printStackTrace();
+			} // try catch
+			
+		} // try catch
+		
+	} // run()
+	
+	void sendMessage(String msg){
+		
+		try{
+			out.writeObject(msg);
+			out.flush();
+			System.out.println("client>" + msg);
+		} catch(IOException ioException){
+			
+			ioException.printStackTrace();
+		} // try catch
+	} // sendMessage()
+	
+	// Main
+	
+	public static void main(String args[]){
+		
+		Requester client = new Requester();
+		client.run();
+		
+	} // main()
+	
+} // class
---
+++ src/server/EchoServer.java
@@ -0,0 +1,101 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+	
+	public static void main(String[] args) throws Exception {
+	  
+	    ServerSocket m_ServerSocket = new ServerSocket(2004,10);
+	    int id = 0;
+	    
+	    while (true) {
+	    	
+	    	System.out.println("Waiting for connection . . .");
+	    	Socket clientSocket = m_ServerSocket.accept();
+	    	ClientServiceThread cliThread = new ClientServiceThread(clientSocket, id++);
+	    	cliThread.start();
+	    	
+	    } // while
+	    
+  } // main()
+	
+} // class
+
+class ClientServiceThread extends Thread {
+	
+	Socket clientSocket;
+	String message;
+	int clientID = -1;
+	boolean running = true;
+	ObjectOutputStream out;
+	ObjectInputStream in;
+	
+	// Constructor
+	ClientServiceThread(Socket s, int i) {
+		
+		clientSocket = s;
+	    clientID = i;
+	    
+	} // ClientServiceThread()
+
+	void sendMessage(String msg){
+		
+		try{
+			out.writeObject(msg);
+			out.flush();
+			System.out.println("client> " + msg);
+			
+		} catch(IOException ioException){
+			
+			ioException.printStackTrace();
+		} // try catch
+		
+	} // sendMessage()
+	
+	public void run() {
+		
+		System.out.println("Accepted Client : ID - " + clientID + " : Address - "
+        + clientSocket.getInetAddress().getHostName());
+		try {
+			out = new ObjectOutputStream(clientSocket.getOutputStream());
+			out.flush();
+			
+			in = new ObjectInputStream(clientSocket.getInputStream());
+			
+			System.out.println("Accepted Client : ID - " + clientID + " : Address - "
+		        + clientSocket.getInetAddress().getHostName());
+		
+			sendMessage("Connection successful");
+			
+			do{
+				try{
+				
+					System.out.println("client>"+clientID+"  "+ message);
+					//if (message.equals("bye"))
+					sendMessage("server got the following: "+message);
+					message = (String)in.readObject();
+				} catch(ClassNotFoundException classnot){
+					
+					System.err.println("Data received in unknown format");
+				} // try catch
+			
+			}while(!message.equals("bye")); // do while
+      
+			System.out.println("Ending Client : ID - " + clientID + " : Address - "
+		        + clientSocket.getInetAddress().getHostName());
+		} catch (Exception e) {
+    	
+			e.printStackTrace();
+		} // try catch
+		
+	} // run()
+} // class
